6cbcd94dc3dad23e67d2fbd1818accfc07edf78d
cptran777 <cptran14@gmail.com>
2016-10-02 12:20:23 -0700

    init and config webpack first draft

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const PATHS = {
+  compiled: path.join(__dirname, 'client/scripts'),
+  src: path.join(__dirname, 'src/main.js')
+};
+
+module.exports = {
+  entry: [
+    PATHS.src
+  ],
+  output: {
+    path: PATHS.compiled,
+    filename: 'app.bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        //exclude: /scripts/,
+        include: __dirname + '/client/src',
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  resolveLoaders: {
+    modulesDirectories: ['node_modules']
+  },
+  resolve: { // in import statements default to these file types if none specified
+    extensions: ['', '.js', '.jsx'],
+    modulesDirectories: ['node_modules']
+  },
+  devServer: {
+    contentBase: PATHS.compiled,
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  plugins: []
+};
