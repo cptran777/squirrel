60b2ff78bdae27cb8ba323e49313ea65bca0dc85
cptran777 <cptran14@gmail.com>
2016-10-02 18:10:26 -0700

    frame code window component

---
+++ src/components/CodeWindow.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import squirrel from './lib/squirrel';
+
+class CodeWindow extends React.component {
+  
+  constructor(props) {
+  	super(props);
+  }
+
+  render() {
+  	return (
+  	  <div></div>
+  	);
+  }
+}
+
+export default Instructions;
+
+
+let myCode = '{ var x = 42; }\n' + 
+   'for (var y = 0; y < x; y++) { \n' + 
+   'x--; \n' +
+   '} \n' + 
+   'while (y < 1000) {\n' +
+   'y++; \n' +
+   '}';
+
+
+
+console.log(squirrel.parse(myCode).should.include(['ForStatement', 'VariableDeclaration']));
+console.log(squirrel.parse(myCode).should.includeWith('ForStatement').nested('ExpressionStatements'));
+
+console.log('we are okay');
