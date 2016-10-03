85bc1107e9492f6ae993436cf400e305a19e0bdb
cptran777 <cptran14@gmail.com>
2016-10-02 18:04:22 -0700

    init CodeWindow and Instructions files. Framed instructions component

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+/******************* COMPONENT IMPORTS *********************/
+
+import Instructions from './Instructions';
+import CodeWindow from './CodeWindow';
+
+/********************** MAIN OBJECT ************************/
+
+// Main application component
+class App extends React.component {
+
+  constructor() {
+
+  }
+
+  render() {
+  	return (
+  	  <div></div>
+  	);
+  }
+
+}
+
+export default App;
